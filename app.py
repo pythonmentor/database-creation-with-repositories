141608eb465201def89d2a0cdbb6de9926e89735
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-06-07 14:57:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
 from demo.models import Product, Store
 from demo.openfoodfacts import ProductDownloader
 
+articles = [
+    {
+        "article_title": "Mon super article au sujet de A",
+        "tags": ["a", "aa", "aaa"],
+        "category": "cat-a"
+    },
+    {
+        "article_title": "Mon second super article au sujet de A",
+        "tags": ["a", "aa", "aaa"],
+        "category": "cat-a"
+    },
+    {
+        "article_title": "Mon super article au sujet de B",
+        "tags": ["b", "bb", "bbb"],
+        "category": "cat-b"
+    },
+    {
+        "article_title": "Mon second super article au sujet de B",
+        "tags": ["b", "bb", "bbb"],
+        "category": "cat-b"
+    },
+    {
+        "article_title": "Mon article général au sujet de A et B",
+        "tags": ["a", "b", "c"],
+        "category": "cat-c"
+    },
+]
 
 def main():
-    downloader = ProductDownloader()
-    products = downloader.fetch('pizza', 500)
-
-    for product in products:
-        try:
-            product = Product.create_from_openfoodfacts(**product)
-        except TypeError:
-            continue
-
-    store = Store.objects.get(name='auchan')
-    print(store)
-    for product in store.products:
-        print(product)
+    for article in articles:
+        article = Article.create_from_openfoodfacts(**article)
 
 
 if __name__ == "__main__":
